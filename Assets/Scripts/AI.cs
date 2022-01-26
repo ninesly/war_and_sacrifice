5fd9900d71ba0311922de6c1f8ab88de09ac7bb1
Maciej Tomaszewski <ninesly@gmail.com>
2022-01-26 21:05:06 +0200

    Basic Enemy Game loop Few scripts for AI

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -15,6 +15,9 @@ public class AI : MonoBehaviour
     [SerializeField] Field enemySacrificeField;
     [SerializeField] CardContainer enemyDiscard;
 
+    [Header("Debug only")]
+    [SerializeField] List<CardManager> cardsInHand = new List<CardManager>();
+
     GameSession gameSession;
     DrawCards drawCardsComponent;
 
@@ -34,15 +37,113 @@ public class AI : MonoBehaviour
     {
         if (gameSession.GetActualUser() != user) return;
         Debug.Log("Aimy: Hey, it's my turn! yupee!");
-        drawCardsComponent.OnClick();
-        var card = enemyMainField.GetComponentInChildren<DragDrop>();
-        if (!card)
-        {
-            Debug.LogError("Aimy: I dont have cards in hand to play!");
-            return;
-        }
-        card.ChangePlaceOfCard(enemyDuelField.gameObject);
+
+        DrawCards();
+        //PlayFirstCard(); // just for testing
+        SortMyCards();
+        FightWithStrongestCard();
+        SacrificeNextStrongestCard();
+        DiscardWeakestCard();
 
         gameSession.NextTurn();
+    }
+
+    // -------------------------------------------------------------------------------- AI ACTIONS
+
+    void DrawCards()
+    {
+        drawCardsComponent.OnClick();
+    }
+
+    void FightWithFirstCard()
+    {
+        var firstCard = enemyMainField.GetComponentInChildren<CardManager>();
+        PlayCard(firstCard, enemyDuelField);
+    }
+
+    void SortMyCards()
+    {
+        
+        CardManager[] allCardsObjects = enemyMainField.GetComponentsInChildren<CardManager>();
+        
+        foreach (CardManager card in allCardsObjects)
+        {
+            cardsInHand.Add(card);
+        }
+
+        cardsInHand.Sort(SortFunc);
+    }
+
+    void FightWithStrongestCard()
+    {
+        PlayCard(0, enemyDuelField);
+    }
+
+    void SacrificeNextStrongestCard()
+    {        
+        PlayCard(0, enemySacrificeField);
+    }
+
+    void DiscardWeakestCard()
+    {
+        var lastIndex = cardsInHand.Count - 1;
+        PlayCard(lastIndex, enemyDiscard);
+    }
+
+    // -------------------------------------------------------------------------------- OTHER
+
+    int SortFunc (CardManager cardA, CardManager cardB)
+    {
+        int valueA = cardA.GetCardSO().cardValue;
+        int valueB = cardB.GetCardSO().cardValue;
+
+
+        if (valueA > valueB)
+        {
+            return -1;
+        } 
+        else if(valueB > valueA)
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+    void PlayCard(int cardIndex, Field field)
+    {
+        var card = cardsInHand[cardIndex];
+        if (!card)
+        {
+            Debug.LogError("Aimy: I dont have card to play!");
+            return;
+        }
+        cardsInHand.RemoveAt(cardIndex);
+
+        var dragDrop = card.GetComponent<DragDrop>();   
+        dragDrop.ChangePlaceOfCard(field.gameObject);
+    }
+    void PlayCard(int cardIndex, CardContainer container)
+    {
+        var card = cardsInHand[cardIndex];
+        if (!card)
+        {
+            Debug.LogError("Aimy: I dont have card to play!");
+            return;
+        }
+        cardsInHand.RemoveAt(cardIndex);
+
+        var dragDrop = card.GetComponent<DragDrop>();
+        dragDrop.ChangePlaceOfCard(container.gameObject);
+    }
+    void PlayCard(CardManager card, Field field)
+    {
+        if (!card)
+        {
+            Debug.LogError("Aimy: I dont have card to play!");
+            return;
+        }
+
+        var dragDrop = card.GetComponent<DragDrop>();
+        dragDrop.ChangePlaceOfCard(field.gameObject);
     }
 }
